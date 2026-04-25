ef4dbb28a6d99ad8acaec35e3e4d10ee26c7f1be
rushikesh <chitkeshwarr@gmail.com>
2026-04-25 23:25:46 +0530

    internal issue resolved

--- Backend/server.js
+++ Backend/server.js
@@ -69,7 +69,7 @@ app.use('/api/contact', contactRoutes);
 
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, '/Frontend/dist')));
-  app.get('*', (req, res) => {
+  app.get('/{*any}', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'Frontend', 'dist', 'index.html'));
   });
 }
